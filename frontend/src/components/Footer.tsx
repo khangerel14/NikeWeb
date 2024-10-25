922ad17f9478215f9774273677680513ca7841ac
Khangerel  Beochir <khangerelbeochir@MacBook-Pro-khaanko.local>
2024-10-25 18:50:15 +0800

    once time rendering

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,3 +1,77 @@
+type Ulti = {
+  title: string;
+  btn: string[];
+};
+
+const data: Ulti[] = [
+  {
+    title: 'Resources',
+    btn: [
+      'Gift Cards',
+      'Find a Store',
+      'Membership',
+      'Nike x NBA',
+      'Nike Journal',
+      'Site Feedback',
+    ],
+  },
+  {
+    title: 'Help',
+    btn: [
+      'Gift Cards',
+      'Find a Store',
+      'Membership',
+      'Nike x NBA',
+      'Nike Journal',
+      'Site Feedback',
+    ],
+  },
+  {
+    title: 'Company',
+    btn: [
+      'Gift Cards',
+      'Find a Store',
+      'Membership',
+      'Nike x NBA',
+      'Nike Journal',
+      'Site Feedback',
+    ],
+  },
+  {
+    title: 'Promotions & Discounts',
+    btn: [
+      'Gift Cards',
+      'Find a Store',
+      'Membership',
+      'Nike x NBA',
+      'Nike Journal',
+      'Site Feedback',
+    ],
+  },
+];
+
 export const Footer = () => {
-  return <div></div>;
+  return (
+    <div className='w-[1344px] flex flex-col mx-auto'>
+      <hr />
+      <div className='flex items-center'>
+        {data.map((e: Ulti, index: number) => {
+          return (
+            <div key={index} className='flex flex-col gap-5 h-96'>
+              <button>{e.title}</button>
+              <div className='flex flex-col gap-2'>
+                {e.btn.map((buttonText, btnIndex) => (
+                  <button key={btnIndex}>{buttonText}</button>
+                ))}
+              </div>
+            </div>
+          );
+        })}
+        <div>
+          <button>United States</button>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
 };
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,84 +1,85 @@
-"use client";
+'use client';
 
-import { Cart, Converse, Favourite, Jordan, Nike } from "@/images";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { Cart, Converse, Favourite, Jordan, Nike } from '@/images';
+import { useRouter } from 'next/navigation';
+import { useState, useEffect } from 'react';
 
 export const Navbar = () => {
-  const [inputValue, setInputValue] = useState("");
+  const router = useRouter();
+  const [values, setValues] = useState('');
   const [open, setOpen] = useState(false);
 
   const openOrNot = () => {
     setOpen(!open);
-    console.log(open);
   };
-  const router = useRouter();
   return (
-    <div className="flex flex-col">
-      <div className="flex items-center h-9 bg-gray-100">
-        <div className="flex justify-between items-center w-[1344px] mx-auto">
-          <div className="flex items-center gap-6">
-            <button onClick={() => router.push("/jordan")}>
+    <div className='flex flex-col'>
+      <div className='flex items-center bg-gray-100 h-9'>
+        <div className='flex justify-between w-[1344px] mx-auto'>
+          <div className='flex items-center gap-6'>
+            <button onClick={() => router.push('/jordan')}>
               <Jordan />
             </button>
-            <button onClick={() => router.push("/converse")}>
+            <button onClick={() => router.push('/converse')}>
               <Converse />
             </button>
           </div>
-          <div className="flex items-center gap-2 text-xs font-semibold">
+          <div className='flex items-center gap-3 text-xs font-semibold'>
             <button
-              className="hover:text-gray-600 border-r pr-2 border-black"
-              onClick={() => router.push("/retail")}
+              className='hover:text-gray-600 border-black pr-3 border-r'
+              onClick={() => router.push('/retail')}
             >
               Find a Store
             </button>
             <button
-              className="hover:text-gray-600 border-r pr-2 border-black"
-              onClick={() => router.push("/help")}
+              className='hover:text-gray-600 border-black pr-3 border-r'
+              onClick={() => router.push('/help')}
             >
               Help
             </button>
             <button
-              className="hover:text-gray-600 border-r pr-2 border-black"
-              onClick={() => router.push("/membership")}
+              className='hover:text-gray-600 border-black pr-3 border-r'
+              onClick={() => router.push('/membership')}
             >
               Join Us
             </button>
             <button
-              className="hover:text-gray-600"
-              onClick={() => router.push("/signin")}
+              className='hover:text-gray-600'
+              onClick={() => router.push('/signin')}
             >
               Sign In
             </button>
           </div>
         </div>
       </div>
-      <div>
-        <div className="flex justify-between items-center w-[1344px] mx-auto h-14">
+      <div className='bg-white'>
+        <div className='flex justify-between w-[1344px] items-center mx-auto h-14'>
           <div>
-            <button onClick={() => router.push("/")}>
+            <button onClick={() => router.push('/')}>
               <Nike />
             </button>
           </div>
-          <div className="flex gap-6 text-md font-semibold items-center pl-44">
-            <button onClick={() => router.push("/new")}>New</button>
-            <button onClick={() => router.push("/men")}>Men</button>
-            <button onClick={() => router.push("/women")}>Women</button>
-            <button onClick={() => router.push("/kids")}>Kids</button>
-            <button onClick={() => router.push("/jordan")}>Jordan</button>
-            <button onClick={() => router.push("/sales")}>Sale</button>
+          <div className='flex items-center gap-6 text-md pl-44 font-semibold'>
+            <button onClick={() => router.push('/new')}>New</button>
+            <button onClick={() => router.push('/men')}>Men</button>
+            <button onClick={() => router.push('/women')}>Women</button>
+            <button onClick={() => router.push('/kids')}>Kids</button>
+            <button onClick={() => router.push('/jordan')}>Jordan</button>
+            <button onClick={() => router.push('/sales')}>Sale</button>
+            <button onClick={openOrNot}>lick mine</button>
           </div>
-          <div className="flex items-center gap-4">
+          <div className='flex items-center gap-4'>
             <input
-              type="text"
-              // value={}
-              className="p-1 px-2 w-40 rounded-full bg-gray-100 outline-none"
-              placeholder="Search..."
+              type='search'
+              className='p-1 w-[170px] px-2 bg-gray-100 rounded-full outline-none'
+              placeholder='Search...'
+              value={values}
+              onChange={(e) => setValues(e.target.value)}
             />
-            <button onClick={() => router.push("/favourites")}>
+            <button onClick={() => router.push('/favourites')}>
               <Favourite />
             </button>
-            <button onClick={() => router.push("/cart")}>
+            <button onClick={() => router.push('/cart')}>
               <Cart />
             </button>
           </div>
