3eb4441289ea806c5f6d8012a6d181c3164e7944
khangerel Be-ochir <khangerelbeochir.com>
2024-11-24 15:39:47 +0800

    on my

--- frontend/src/components/carousel.tsx
+++ frontend/src/components/carousel.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import { Card, CardContent } from '@/components/ui/card';
+import { v4 as uuidv4 } from 'uuid';
 import {
   Carousel,
   CarouselContent,
@@ -14,7 +15,7 @@ export function CarouselDemo() {
     <Carousel className='mx-auto pt-10 w-full max-w-xs'>
       <CarouselContent>
         {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={length}>
+          <CarouselItem key={uuidv4()}>
             <div className='p-1'>
               <Card>
                 <CardContent className='flex aspect-square items-center justify-center p-6'>
