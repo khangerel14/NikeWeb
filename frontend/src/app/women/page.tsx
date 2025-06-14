fe7f54b772d27ddb8e7944a26f010e65f7e1659f
khangerel Be-ochir <khangerelbeochir.com>
2025-06-15 00:31:09 +0800

    fa

--- frontend/src/app/women/page.tsx
+++ frontend/src/app/women/page.tsx
@@ -1,13 +1,11 @@
 'use client';
 
 import { Footer } from '@/features';
-import { Paginations } from '@/components';
 import ClothesContextProvider from '@/context/ClothesContext';
 
 const Page = () => {
   return (
     <ClothesContextProvider>
-      <Paginations />
       <Footer />
     </ClothesContextProvider>
   );
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,8 +2,6 @@ export * from './help-hover';
 
 export * from './custom-card';
 
-export * from './paginations';
-
 export * from './custom-accordion';
 
 export * from './carousel-spacing';
